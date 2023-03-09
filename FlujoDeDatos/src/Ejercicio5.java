b6264fb3b19884eba4f406c54ec3e5b29cca18c7
Aitor Iglesias <iglesiaspubillaitor@gmail.com>
2023-03-09 11:17:01 +0100

    Ejercicio 5 Terminado

---
+++ FlujoDeDatos/src/Ejercicio5.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author aitor
+ *
+ */
+public class Ejercicio5 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("EJERCICIO 5 - DIVISIBLE POR 2");
+		System.out.println("Introduce un numero: ");
+		int num = sc.nextInt();
+		if(num%2 == 0) {
+			System.out.println("El numero SI que es divisible por 2");
+		} else {
+			System.out.println("El numero NO es divisible por 2");
+		}
+
+	}
+
+}
