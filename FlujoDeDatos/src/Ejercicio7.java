862d302aa0551cad26536ed634ea3adce35e811f
Aitor Iglesias <iglesiaspubillaitor@gmail.com>
2023-03-09 11:32:03 +0100

    Ejercicio 7 Terminado

---
+++ FlujoDeDatos/src/Ejercicio7.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+/**
+ * @author aitor
+ *
+ */
+public class Ejercicio7 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("EJERCICIO 7 - PRIMEROS 100 numeros");
+		int num = 1;
+		while(num<=100) {
+			System.out.println(num);
+			num++;
+		}
+
+	}
+
+}
