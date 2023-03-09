cd13a58472bf4068353802d9d32617d04113eea1
Aitor Iglesias <iglesiaspubillaitor@gmail.com>
2023-03-09 10:59:30 +0100

    Ejercicio 2 Terminado Ejercicio 3 Terminado

---
+++ FlujoDeDatos/src/Ejercicio2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author aitor
+ *
+ */
+public class Ejercicio2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("EJERCICIO 2 - CONCATENAR STRING");
+		System.out.println("Introduce tu nombre: ");
+		String name = sc.nextLine();
+		System.out.println("BIENVENIDO" +name);
+
+	}
+
+}
---
+++ FlujoDeDatos/src/Ejercicio3.java
@@ -0,0 +1,26 @@
+import javax.swing.JOptionPane;
+
+/**
+ * 
+ */
+
+/**
+ * @author aitor
+ *
+ */
+public class Ejercicio3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("EJERCICIO 3 - UTILIZANDO JOPTIONPANE");
+		String nombre = JOptionPane.showInputDialog("Introduce tu nombre");
+		JOptionPane.showMessageDialog(null, "Bienvenido "+nombre);
+
+	}
+
+}
