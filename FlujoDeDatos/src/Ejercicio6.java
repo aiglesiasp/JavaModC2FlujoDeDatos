741d19fc0383bc524acd43b33ac8baab173e1f96
Aitor Iglesias <iglesiaspubillaitor@gmail.com>
2023-03-09 11:27:41 +0100

    Ejercicio 6 Terminado

---
+++ FlujoDeDatos/src/Ejercicio6.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author aitor
+ *
+ */
+public class Ejercicio6 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		final double IVA = 0.21;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("EJERCICIO 6 - CALCULAR PRECIO CON IVA");
+		System.out.println("Introduce el precio del producto: ");
+		double precioInicial = sc.nextDouble();
+		double precioFinal = precioInicial + (precioInicial * IVA);
+		
+		System.out.println("El precio final del producto es de: "+precioFinal);
+		
+
+	}
+
+}
